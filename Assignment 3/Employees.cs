3ccc8879af83a123a92f7f916e6dc795ac177f18
YoussefAmrAbdelmoneim <yosefamr200@gmail.com>
2025-08-04 00:15:05 +0300

    Q3

--- Assignment 3/Employees.cs
+++ Assignment 3/Employees.cs
@@ -4,12 +4,55 @@ namespace Assignment_3
 {
     internal class Employees
     {
+        #region Attributes
+        private int id;
+        private string? name;
+        private SecurityLevel security;
+        private decimal salary;
+        private HiringDate hireDate;
         private char gender;
-        public int ID { get; set; }
-        public string? Name { get; set; }
-        public SecurityLevel Security { get; set; }
-        public decimal Salary { get; set; }
-        public DateTime HireDate { get; set; }
+        #endregion
+        #region Constructor
+        public Employees(int id, string? name, SecurityLevel security, decimal salary, HiringDate hireDate, char gender)
+        {
+            this.id = id;
+            this.name = name;
+            this.security = security;
+            this.salary = salary;
+            this.hireDate = hireDate;
+            this.gender = gender;
+        }
+        #endregion
+        #region Properties
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string? Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public SecurityLevel Security
+        {
+            get { return security; }
+            set { security = value; }
+        }
+
+        public decimal Salary
+        {
+            get { return salary; }
+            set { salary = value >= 0 ? value : 0; }
+        }
+
+        public HiringDate HireDate
+        {
+            get { return hireDate; }
+            set { hireDate = value ; }
+        }
 
         public char Gender
         {
@@ -25,10 +68,85 @@ namespace Assignment_3
                 }
             }
         }
-        public override string ToString()
+        #endregion
+        #region Methods
+        public void SetId(int id)
         {
-            return $"Id={ID}\nName={Name}\nGender={gender}\nSecurity={Security}\nSalary={string.Format(CultureInfo.CurrentCulture, "{0:C}", Salary)}\nHireDate={HireDate}";
+            this.id = id;
         }
 
+        public int GetId()
+        {
+            return id;
+        }
+
+        public void SetName(string name)
+        {
+            if (name is not null)
+            {
+                this.name = name;
+            }
+            else
+            {
+                Console.WriteLine("Invalid, Because of Null");
+            }
+        }
+
+        public string? GetName()
+        {
+            return name;
+        }
+
+        public void SetSecurity(SecurityLevel security)
+        {
+            this.security = security;
+        }
+
+        public SecurityLevel GetSecurity()
+        {
+            return security;
+        }
+
+        public void SetSalary(decimal salary)
+        {
+            if (salary >= 0)
+                this.salary = salary;
+            else
+                Console.WriteLine("Invalid Because of negative");
+        }
+
+        public decimal GetSalary()
+        {
+            return salary;
+        }
+
+        public void SetHireDate(HiringDate hiringDate)
+        {
+                this.hireDate = hiringDate;
+        }
+
+        public HiringDate GetHireDate()
+        {
+            return hireDate;
+        }
+
+        public void SetGender(char gender)
+        {
+            if (gender == 'M' || gender == 'F')
+                this.gender = gender;
+            else
+                Console.WriteLine("Gender must be M or F ");
+        }
+
+        public char GetGender()
+        {
+            return gender;
+        }
+
+        public override string ToString()
+        {
+            return $"Id={id}\nName={name}\nGender={gender}\nSecurity={security}\nSalary={string.Format(CultureInfo.CurrentCulture, "{0:C}", salary)}\nHireDate={hireDate}";
+        }
+        #endregion
     }
 }
--- Assignment 3/Program.cs
+++ Assignment 3/Program.cs
@@ -5,20 +5,34 @@
         static void Main(string[] args)
         {
             #region Q1
-            //Employees employees = new Employees();
-            //employees.Name = "Youssef";
-            //employees.ID = 1;
-            //employees.Gender = 'M';
-            //employees.HireDate= DateTime.Now;
-            //employees.Security = SecurityLevel.Developer;
-            //employees.Salary = 20000;
+            //HiringDate hiringDate = new HiringDate(3,8,2025);
+            //Employees employees = new Employees(1, "Youssef", SecurityLevel.Developer, 20000,hiringDate,'M');
             //Console.WriteLine(employees);
             #endregion
             #region Q2
-            //HiringDate hiringDate = new HiringDate();
-            //hiringDate.Day = 1;
-            //hiringDate.Month = 7;
-            //hiringDate.Year = 2025;
+            //HiringDate hiringDate = new HiringDate(4,8,2005);
+            //Console.WriteLine(hiringDate);
+            #endregion
+            #region Q3
+            //HiringDate hiringDate = new HiringDate(3, 8, 2025);
+            //Employees[] EmpArr = new Employees[3];
+            //try
+            //{
+            //    EmpArr =
+            //    [
+            //        new Employees(1,"Youssef", SecurityLevel.DBA, 40000, hiringDate, 'M'),
+            //    new Employees(2, "Amr", SecurityLevel.Guest, 30000, hiringDate, 'M'),
+            //    new Employees(3, "Ali", SecurityLevel.Secretary, 20000, hiringDate, 'M'),
+            //];
+            //}
+            //catch
+            //{
+            //    Console.WriteLine("Error");
+            //}
+            //for (int i = 0; i < EmpArr.Length; i++)
+            //{
+            //    Console.WriteLine(EmpArr[i]);
+            //}
             #endregion
         }
     }
