02682572a6b4d8ae1f35685cc28761cdaea38191
YoussefAmrAbdelmoneim <yosefamr200@gmail.com>
2025-08-03 22:16:34 +0300

    Class

---
+++ Demo/Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class Car
+    {
+        #region attributes
+        private int id;
+        private string? model;
+        private double speed;
+        #endregion
+        #region properties
+        public string? Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+        public double Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+        #endregion
+        #region constructor
+        public Car(int id, string? model, double speed)
+        {
+            this.id = id;
+            this.model = model;
+            this.speed = speed;
+        }
+        #endregion
+    }
+}
