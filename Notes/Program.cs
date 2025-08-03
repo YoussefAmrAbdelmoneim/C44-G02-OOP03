8d691bda153ae5262921ca7cc0cef5e135c6cd2e
YoussefAmrAbdelmoneim <yosefamr200@gmail.com>
2025-08-04 00:17:15 +0300

    Class vs Struct

--- Notes/Program.cs
+++ Notes/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Class vs Struct
+            /*Class vs Struct
+             * Class Supports inheritance , Struct Does not support inheritance
+             * Class Can be Null , Struct Cannot be null (unless nullable).
+             * Class Memory Managed by the garbage collector  , Struct Memory doesn't need garbage collection .
+             * Class Constructor : Compiler Will Generate Empty Parameterless Constructor unless you define a user defined constructor
+             * Struct Constructor : Compiler Will Generate Always Generate Default Parameterless Constructor Even if you define a user defined constructor
+             */
+            #endregion
         }
     }
 }
